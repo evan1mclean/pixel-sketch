33c9d199602ba2d8a8f74f23dc5dbc8c5b422375
Evan McLean <evanhughmclean@gmail.com>
2022-04-26 17:19:25 -0400

    Adds background color function

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
 let dimension = document.getElementById('gridSlider').value;
 let color = document.getElementById('penColor').value;
+let bgColor = document.getElementById('bgColor').value;
 
-function makeGrid(dimension) {
+function makeGrid(dimension, bgColor) {
     let sketchpad = document.querySelector('.sketchpad');
     let gridSize = document.getElementById('gridSize');
     gridSize.textContent = `Grid Size: ${dimension} x ${dimension}`;
@@ -10,6 +11,8 @@ function makeGrid(dimension) {
     for (let i =0; i < (dimension * dimension); i++) {
         const grid = document.createElement('div');
         grid.classList.add('grid');
+        grid.classList.add('background');
+        grid.style.backgroundColor = bgColor;
         sketchpad.appendChild(grid);
     }
 }
@@ -18,9 +21,11 @@ function draw(color) {
     let grid = document.querySelectorAll('.grid');
     grid.forEach ((pixel) => pixel.addEventListener('mouseover', function(e) {
         pixel.addEventListener('mousedown', function() {
+            pixel.classList.remove('background');
             pixel.style.backgroundColor = color;
         });
         if (e.buttons == 1) {
+            pixel.classList.remove('background');
             pixel.style.backgroundColor = color;
         }
     }));
@@ -41,12 +46,23 @@ function changeGridSize() {
         sketchpad = document.createElement('div');
         sketchpad.classList.add('sketchpad');
         container.appendChild(sketchpad);
-        makeGrid(e.target.value);
+        bgColor = document.getElementById('bgColor').value;
+        makeGrid(e.target.value, bgColor);
         color = document.getElementById('penColor').value;
         draw(color);
     });
 }
 
+function changeBackgroundColor() {
+    document.getElementById('bgColor').addEventListener('input', (e) => {
+        let grid = document.querySelectorAll('.background');
+        grid.forEach((cell) => {
+            cell.style.backgroundColor = `${e.target.value}`;
+        })
+    });
+}
+
 makeGrid(dimension);
+changeBackgroundColor();
 changeGridSize();
 penColor();
