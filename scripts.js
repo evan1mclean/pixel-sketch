8d53ab90a7cfc64c38d709d15ef84f295393d0f9
Evan McLean <evanhughmclean@gmail.com>
2022-04-28 13:21:33 -0400

    Fix bugs introduced with eraser and changed toggle function

--- scripts.js
+++ scripts.js
@@ -33,7 +33,7 @@ function clearGrid() {
         color = document.getElementById('penColor').value;
         bgColor = document.getElementById('bgColor').value;
         dimension = document.getElementById('gridSlider').value;
-        toggleButton('eraser');
+        toggleButtonOff();
         makeGrid(dimension, bgColor);
         draw(color);
     })
@@ -54,10 +54,12 @@ function draw(color) {
 }
 
 function changePenColor() {
+    color = document.getElementById('penColor').value;
     draw(color);
     document.getElementById('penColor').addEventListener('input', (e) => {
-    draw(e.target.value);
-});
+        toggleButtonOff();
+        draw(e.target.value);
+    });
 }
 
 function changeGridSize() {
@@ -72,43 +74,45 @@ function changeGridSize() {
 
 function changeBackgroundColor() {
     document.getElementById('bgColor').addEventListener('input', (e) => {
+        toggleButtonOff();
         let grid = document.querySelectorAll('.background');
         grid.forEach((cell) => {
             cell.style.backgroundColor = `${e.target.value}`;
         })
+        changePenColor();
     });
 }
 
-function toggleButton(button) {
-    let selected = document.querySelector(`.${button}`);
-    if (selected.classList.contains(selected)) {
+function toggleButtonOff() {
+    let selected = document.querySelector('button');
+    if (selected.classList.contains('selected') === true) {
         selected.classList.remove('selected');
-    }
-    else {
-        selected.classList.toggle('selected');
     }
 }
 
 function eraser() {
     let btn = document.querySelector('.eraser');
         btn.addEventListener('click', (e) => {
-            toggleButton('eraser');
-            bgColor = document.getElementById('bgColor').value;
-            let grid = document.querySelectorAll('.grid');
-            grid.forEach ((pixel) => pixel.addEventListener('mouseover', function(e) {
-                pixel.addEventListener('mousedown', function() {
-                    pixel.classList.add("background");
-                    pixel.style.backgroundColor = bgColor;
-                });
-                if (e.buttons == 1) {
-                    pixel.classList.add("background");
-                    pixel.style.backgroundColor = bgColor;
-                }
-            }));
+            if (btn.classList.contains('selected')) {
+                toggleButtonOff();
+            }
+            else {
+                btn.classList.add('selected');
+                bgColor = document.getElementById('bgColor').value;
+                let grid = document.querySelectorAll('.grid');
+                grid.forEach ((pixel) => pixel.addEventListener('mouseover', function(e) {
+                    pixel.addEventListener('mousedown', function() {
+                        pixel.classList.add("background");
+                        pixel.style.backgroundColor = bgColor;
+                    });
+                    if (e.buttons == 1) {
+                        pixel.classList.add("background");
+                        pixel.style.backgroundColor = bgColor;
+                    }
+                }));
+            }
         });
-    changeBackgroundColor();
 }
-
 makeGrid(dimension);
 changeBackgroundColor();
 changeGridSize();
